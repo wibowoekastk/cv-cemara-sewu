2f54871bf5fdebe01f803d6dfcef8e3c95460fee
wibowoekastk <wibowochannel24@gmail.com>
2026-03-02 08:12:02 +0700

    kami

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -171,7 +171,7 @@
                 
                 <div>
                     <h2 class="text-gold-600 font-bold tracking-[0.2em] uppercase text-sm mb-6 flex items-center gap-3">
-                        <span class="w-10 h-0.5 bg-gold-600"></span> Tentang Kami
+                        <span class="w-10 h-0.5 bg-gold-600"></span> Tentang Kamu
                     </h2>
                     <h3 class="text-4xl md:text-5xl font-poppins font-bold text-cemara-950 mb-8 leading-tight">Dedikasi Modernisasi <br>Industri Peternakan</h3>
                     <p class="text-gray-600 mb-8 leading-relaxed text-lg text-justify">
